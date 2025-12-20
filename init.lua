ea900e57b7840d88e5ac7cdc4f0083fdc79ab09a
Javier Franco <notfrannco@gmail.com>
2025-12-20 13:28:51 -0300

    update

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
-require 'core.options'
-require 'core.keymaps'
+--require 'core.options'
+--require 'core.keymaps'
+require 'config.options'
+require 'config.keymaps'
+require 'config.autocmds'
+require 'core.lazy'
+require 'core.lsp'
 
 -- remove padding 
 vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
@@ -14,31 +19,31 @@ vim.api.nvim_create_autocmd("UILeave", {
   callback = function() io.write("\027]111\027\\") end,
 })
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+--local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+--if not (vim.uv or vim.loop).fs_stat(lazypath) then
+--  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+--  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+--  if vim.v.shell_error ~= 0 then
+--    error('Error cloning lazy.nvim:\n' .. out)
+--  end
+--end
+--vim.opt.rtp:prepend(lazypath)
 
 
-require('lazy').setup({
-  require 'plugins.colortheme',
-  require 'plugins.neotree',
-  require 'plugins.bufferline',
-  require 'plugins.lualine',
-  require 'plugins.treesitter',
-  require 'plugins.telescope',
-  require 'plugins.cmd',
-  require 'plugins.lsp',
-  require 'plugins.gitsigns',
-  require 'plugins.indent-blankline',
-  require 'plugins.misc',
-  require 'plugins.trouble'
-})
+--require('lazy').setup({
+--  require 'plugins.colortheme',
+--  require 'plugins.neotree',
+--  require 'plugins.bufferline',
+--  require 'plugins.lualine',
+--  require 'plugins.treesitter',
+--  require 'plugins.telescope',
+--  require 'plugins.cmd',
+--  require 'plugins.lsp',
+--  require 'plugins.gitsigns',
+--  require 'plugins.indent-blankline',
+--  require 'plugins.misc',
+--  require 'plugins.trouble'
+--})
 
 
 --colorscheme catppuccin " catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
@@ -46,26 +51,26 @@ require('lazy').setup({
 vim.cmd.colorscheme "catppuccin-macchiato"
 
 --  New on Neovim 0.11
-vim.diagnostic.config({
-    virtual_lines = true,
-    --virtual_text = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
-    float = {
-        border = "rounded",
-        source = true,
-    },
-    signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = "󰅚 ",
-            [vim.diagnostic.severity.WARN] = "󰀪 ",
-            [vim.diagnostic.severity.INFO] = "󰋽 ",
-            [vim.diagnostic.severity.HINT] = "󰌶 ",
-        },
-        numhl = {
-            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-            [vim.diagnostic.severity.WARN] = "WarningMsg",
-        },
-    },
-})
+--vim.diagnostic.config({
+--    virtual_lines = true,
+--    --virtual_text = true,
+--    underline = true,
+--    update_in_insert = false,
+--    severity_sort = true,
+--    float = {
+--        border = "rounded",
+--        source = true,
+--    },
+--    signs = {
+--        text = {
+--            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+--            [vim.diagnostic.severity.WARN] = "󰀪 ",
+--            [vim.diagnostic.severity.INFO] = "󰋽 ",
+--            [vim.diagnostic.severity.HINT] = "󰌶 ",
+--        },
+--        numhl = {
+--            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+--            [vim.diagnostic.severity.WARN] = "WarningMsg",
+--        },
+--    },
+--})
